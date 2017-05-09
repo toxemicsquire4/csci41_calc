b463de57b8097feb58e0a15ea77e6eafdb0ff9bf
deadmemeconnoisseur42 <ax.ku195@gmail.com>
2017-05-08 22:17:27 -0700

    Update Division.cc

--- Division.cc
+++ Division.cc
@@ -1,21 +1,9 @@
 #include <iostream>
 using namespace std;
-
-//old wrong version, ignore this
-/*int main() {
-    double x, y, result;
-    cout << "Enter a number: " << endl;
-    cin >> x;
-    cout << "Enter the number you'll divide the first number by: " << endl;
-    cin >> y;
-
-    result = x/y;
-    cout << x << " divided by " << y << " equals " << result << endl;
-    return 0;
-}*/
+//deleted the old version
 
 //This is the working code, or it should be
-float division(Normal& x, Normal& y){
+float division(Number& x, Number& y){ //changed from Normal to Number
     if (y.get_value() == 0) user_error();
     float result = x.get_value()/y.get_value();
     return result;
