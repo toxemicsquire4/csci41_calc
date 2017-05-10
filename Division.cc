6a11ac695b690bb494af1346fef48f97781e111c
Elsie <eangelcortes@live.com>
2017-05-10 10:15:46 -0700

    Update Division.cc

--- Division.cc
+++ Division.cc
@@ -4,7 +4,6 @@ using namespace std;
 
 //This is the working code, or it should be
 float division(Number& x, Number& y){ //changed from Normal to Number
-    if (y.get_value() == 0) user_error();
     float result = x.get_value()/y.get_value();
     return result;
 }
