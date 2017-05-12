a0eee2988f970baed330c09d5c6a09680dbfab97
deadmemeconnoisseur42 <ax.ku195@gmail.com>
2017-05-11 21:42:32 -0700

    Update Division.cc

--- Division.cc
+++ Division.cc
@@ -1,10 +1,9 @@
 #include <iostream> //Do NOT USE, LEGACY, please use main.cc
 
 using namespace std;
-//deleted the old version
 
 //This is the working code, or it should be
 float division(Number& x, Number& y){ //changed from Normal to Number
-    float result = x.get_value()/y.get_value();
-    return result;
+    float result = x.get_value()/y.get_value(); //divides the Number&x value by the Number&y value
+    return result; //just like it says, it returns the resulting number of the equation
 }
