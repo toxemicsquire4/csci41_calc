4756fb2bbd9390522949a60d5a6a06726c2bcd7e
Palmoon <kristian.erl12@gmail.com>
2017-05-12 02:18:34 -0700

    whatitdoes
    Makes X and Y objects of Number class, then stores the quotient into result, and returns result.

--- Division.cc
+++ Division.cc
@@ -6,3 +6,4 @@ float division(Number& x, Number& y){ //changed from Normal to Number
     float result = x.get_value()/y.get_value(); //divides the Number&x value by the Number&y value
     return result; //just like it says, it returns the resulting number of the equation
 }
+// notes
